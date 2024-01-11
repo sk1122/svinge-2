c3810cffa0a36aa4477db6b687b58751e4b686eb
ac2f <ww>
2024-01-11 05:28:13 +0300

    Minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './round-robin';
-export * from './dynamic-balance';
-export * from "./round-robin-ws";
+export * from './round-robin'
+export * from './dynamic-balance'
+export * from "./round-robin-ws"
